29e30636a38b7b0a7f6fd091e9618190e34112f4
Olivier Avande <mavande@unomaha.edu>
2018-03-03 22:39:27 -0600

    data between activities

--- AndroidIntents/app/src/main/java/com/example/oliavd/androidintents/CreateUserActivity.java
+++ AndroidIntents/app/src/main/java/com/example/oliavd/androidintents/CreateUserActivity.java
@@ -1,13 +1,65 @@
 package com.example.oliavd.androidintents;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class CreateUserActivity extends AppCompatActivity {
+
+    private EditText userName;
+    private boolean male;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_user_activity);
+        userName = findViewById(R.id.editText);
+        final RadioGroup rg = findViewById(R.id.user_name);
+
+
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.male:
+                        male = true;
+
+                        break;
+                    case R.id.female:
+                        male = false;
+
+                        break;
+                }
+
+                System.out.println(male);
+
+
+            }
+
+
+        });
     }
+
+
+    public void onClick(View view) {
+        finish();
+    }
+
+    @Override
+    public void finish() {
+
+        //on finish, create new intent and send data to callee activity
+        Intent intent = new Intent();
+        intent.putExtra(User.USER_NAME, userName.getText().toString());
+        intent.putExtra(User.USER_GENDER, male);
+        setResult(RESULT_OK, intent);
+
+        super.finish();
+    }
+
 }
+
--- AndroidIntents/app/src/main/java/com/example/oliavd/androidintents/UserOverviewActivity.java
+++ AndroidIntents/app/src/main/java/com/example/oliavd/androidintents/UserOverviewActivity.java
@@ -1,21 +1,76 @@
 package com.example.oliavd.androidintents;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class UserOverviewActivity extends AppCompatActivity {
+
+    public static final int SUB_ACTIVITY_CREATE_USER = 10;
+    private User user;
+    ImageView genderImg;
+    TextView name;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_overview);
 
+        /* TODO check persistence if user exists and load the existing one */
+
+        boolean userExists = false;
+        genderImg = (ImageView) findViewById(R.id.genderImage) ;
+        name = findViewById(R.id.user_name);
+
+        /*Create a new one if none exist (call subactivity CreateActivity*/
+        if(!userExists){
+            Intent intent = new Intent(this,CreateUserActivity.class);
+            startActivityForResult(intent, SUB_ACTIVITY_CREATE_USER);
+        }
+
             }
 
+        /* Callback for started sub-activity */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
 
+
+        if (requestCode == SUB_ACTIVITY_CREATE_USER && resultCode == Activity.RESULT_OK){
+            Bundle extras = data.getExtras();
+            if (extras != null){
+                String name = extras.getString(User.USER_NAME);
+                boolean gender = extras.getBoolean(User.USER_GENDER);
+
+                user = new User(name,gender);
+                updateUserInterface();
+            }
+        }
+    }
+
+    private void updateUserInterface(){
+        //TODO show the new user values in the UI
+        System.out.println(user.gender);
+        if (user.gender){
+
+            genderImg.setImageDrawable(getDrawable(R.drawable.ic_boy));
+        }else{
+            genderImg.setImageDrawable(getDrawable(R.mipmap.ic_launcher_foreground));
+        }
+
+
+        name.setText(user.name);
+
+
+    }
 
 }
